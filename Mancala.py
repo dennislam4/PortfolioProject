cc06edf9491f2155023c71a8dd728741f9092033
Dennis <lamden@oregonstate.edu>
2022-12-02 21:15:54 -0700

    Completed Mancala class, removed empty methods that were not needed, changed some logic in the Board class by changing the player boards to a single list to match readme criteria

--- Mancala.py
+++ Mancala.py
@@ -3,14 +3,6 @@
 # Date: 11-20-2022
 # Description: A text based game version of the popular board game Mancala.
 
-print("Welcome to Mancala!\n\nThe objective of the game is to collect as many seeds as you can in your store!")
-print("Players will pick up seeds from their pits and sow them to the right, placing one seed in each pit along the way.")
-print("When you cross to your store, you stow away a seed but if you cross your oppenent's store, you skip over to their pit!")
-print("Switch turns when you run out of seeds, if your last seed lands in your store, gain a free turn!")
-print("If the last seed lands in your own own empty pit adjacent to the opponent's pit, you may capture their seeds into your pit!")
-print("Good luck and have fun!\n")
-
-
 class Player:
     """Class object that represents the two players playing the game."""
 
@@ -34,9 +26,11 @@ class Board:
         """Initializes Board class and its data members, the number of pits and seeds within the Mancala game board."""
         self._seeds = seeds
         self._pits = pits
-        self._board = [seeds for _ in range(pits)]
-        self._P1_store = 0
-        self._P2_store = 0
+
+        # initialize self-board-list, the main data structure.
+        # sample: [4, 4, 4, 4, 4, 4, 0, 4, 4, 4, 4, 4, 4, 0]
+        # index 0-5 will be player1's pit, index 7-12 will be player2's pit and index 6,13 will be storage
+        self._playboard = [self._seeds if i % 7 != 6 else 0 for i in range((self._pits+1) * 2)]
 
     def get_seeds(self):
         """Get method for physical Mancala seeds that players move and play with."""
@@ -48,20 +42,30 @@ class Board:
 
     def get_P1_store(self):
         """Get method for player1 storage. Used for point collection"""
-        return self._P1_store
+        return self._playboard[6]
 
     def get_P2_store(self):
         """Get method for player2 storage. Used for point collection"""
-        return self._P2_store
+        return self._playboard[13]
 
     def get_board(self):
         """Get method for the Mancala game board."""
-        return self._board
+        return self._playboard
+
+    def get_P1_board(self):
+        """Get method for the Mancala game board for player 1."""
+        return self._playboard[0:6]
+
+    def get_P2_board(self):
+        """Get method for the Mancala game board for player 2."""
+        return self._playboard[7:13]
 
     def get_seeds_in_pit(self, player_id):
         """Get method to return the number of seeds player pits"""
-        for pit in self.get_pits(player_id):
-            return pit.get_seeds()
+        if player_id == 1:
+            return sum(self._playboard[0:6])
+        if player_id == 2:
+            return sum(self._playboard[7:13])
 
     def check_pit(self, player_id):
         """
@@ -80,35 +84,25 @@ class Mancala:
     """
 
     def __init__(self):
-        self._players = {1: None, 2: None}
+        # self._players = {1: None, 2: None}
         self._board = Board(seeds=4, pits=6)
-        self._P1_board = Board(seeds=4, pits=6)
-        self._P2_board = Board(seeds=4, pits=6)
+        self._players = []
 
     def create_player(self, name):
         """Method that creates a player for the Mancala game"""
-        self._players[1] = Player(name)
-        self._players[2] = Player(name)
+        self._players.append(Player(name))
 
     def print_board(self):
         """Prints the current board information including the number of seeds in all pits and storages."""
-        store_num_one = self._P1_board.get_P1_store()
-        store_num_two = self._P2_board.get_P2_store()
+        store_num_one = self._board.get_P1_store()
+        store_num_two = self._board.get_P2_store()
         print("CURRENT BOARD:\n")
         print("player1:")
         print("store: ", store_num_one)
-        print(self._P1_board.get_board())
+        print(self._board.get_board()[0:6])
         print("\nplayer2:")
-        print(self._P2_board.get_board())
+        print(self._board.get_board()[7:13])
         print("store: ", store_num_two)
-
-    def new_game(self):
-        """Creates fresh start for players to play the game"""
-        player_one = input("Player 1, what is your name?")
-        self.create_player(player_one)
-        player_two = input("Player 2, what is your name?")
-        self.create_player(player_two)
-        self.print_board()
 
     def play_game(self, player_index, pit_index):
         """
@@ -116,51 +110,73 @@ class Mancala:
         and updates the number of seeds for each pit and store belonging to the players. Checks the state of the game if
         it has ended. If game has ended, returns 'Game is ended'. Returns list at the end of current seed number and
         stores indicating which player they belong to.
-         """
-
-        if self._board.end_game():
-            return "Game is ended"
-
+        """
         if pit_index > 6 or pit_index <= 0:
             return "Invalid number for pit index"
 
+        elif self._board.check_pit(1) or self._board.check_pit(2):
+            return "Game is ended"
 
+        else:
+            # player I
+            if player_index == 1:
+                # declare a variable "sow" -> number of seed player get
+                sow = self._board.get_board()[pit_index - 1]
+                self._board.get_board()[pit_index - 1] = 0
+                # set a variable save_i to record the last i in the next for loop in order to avoid referenced before assignment
+                save_i = 0
+                for i in range(sow):
+                    self._board.get_board()[pit_index + i] += 1
+                    save_i = i
+                # special case 1, the final move is landed in storage
+                if pit_index + save_i == 6:
+                    print("player 1 takes another turn")
 
-    def store_seeds(self, seeds):
-        """Method used to store the seeds in player storage."""
+                # special case 2, the final move is landed in an empty pit
+                elif pit_index + save_i < 6 and self._board.get_board()[pit_index + save_i] == 1:
+                    self._board.get_board()[pit_index + save_i] = 0
+                    self._board.get_board()[6] += 1
+                    self._board.get_board()[6] += self._board.get_board()[pit_index + save_i + 7]
+                    self._board.get_board()[pit_index + save_i + 7] = 0
+            # player II
+            else:
+                sow = self._board.get_board()[pit_index - 1 + 7]
+                self._board.get_board()[pit_index - 1 + 7] = 0
+                for i in range(sow):
+                    adjust_index = pit_index + i + 7
+                    if adjust_index > 13:
+                        adjust_index = adjust_index - 14
+                    self._board.get_board()[adjust_index] += 1
 
-    def capture_seeds(self, seeds):
-        """Method used for the special rule of capturing seeds"""
+                if adjust_index == 13:
+                    print("player 2 takes another turn")
+                # special case 2, the final move is landed in an empty pit
+                elif adjust_index > 7 and self._board.get_board()[adjust_index] == 1:
+                    self._board.get_board()[adjust_index] = 0
+                    self._board.get_board()[13] += 1
+                    self._board.get_board()[13] += self._board.get_board()[adjust_index - 7]
+                    self._board.get_board()[adjust_index - 7] = 0
 
-    def player_options(self, pits):
-        """Checks board for all the legal and valid possible options they are allowed to do for their turn."""
-
-    def move_seeds(self, seeds):
-        """
-        Moves seeds to each pit and/or player storage based onthe option the player chooses from the player_options
-        method. Userinput will be needed in order to make moves. The player grabs all their seeds from the pit they
-        have selected and then begins moving towards the right, placing a single seed in each pit for each respective
-        step. If the player encounters their storage, they place a seed in that storage. Players are not allowed to
-        place seeds in theiropposing player’s storage.
-        """
-
-    def end_turn(self):
-        """Ends the turn of the current player and switches to the opposing/next player for them to begin their turn."""
+        return self._board.get_board()
 
     def return_winner(self):
         """
         When game has ended, the winner is returned. If there is a tie, returns a message indicating the game has
         ended in a tie. If the game is still running, prints a message that the game has not yet ended.
         """
-        player_one_score = self._board.get_points(1)
-        player_two_score = self._board.get_points(2)
-        if player_one_score > player_two_score:
-            return "Winner is player 1"
-        elif player_two_score > player_one_score:
-            return "Winner is player 2"
-        elif player_one_score == player_two_score:
-            return "It's a tie"
+        if self._board.check_pit(1) or self._board.check_pit(2):
+            player_one_score = self._board.get_P1_store()
+            player_two_score = self._board.get_P2_store()
+            if player_one_score > player_two_score:
+                return "Winner is player 1 " + self._players[0].get_name()
+            elif player_two_score > player_one_score:
+                return "Winner is player 2 " + self._players[1].get_name()
+            elif player_one_score == player_two_score:
+                return "It's a tie"
+        else:
+            return "Game is not ended"
 
     def end_game(self):
         """Stops the game."""
         pass
+
